45b1582e46e655a536f3ec78cfccd22b832117e5
minicono <liangxp2360@163.com>
2022-03-11 20:09:55 +0800

    fix bug: miss logs within the same url

--- ZeekKnowledge/spiders/zeekknowledge.py
+++ ZeekKnowledge/spiders/zeekknowledge.py
@@ -23,30 +23,25 @@ class ZeekknowledgeSpider(scrapy.Spider):
             log_desc = self.text_clean(entry.xpath("../../../td[2]/p/text()").extract_first())
             log_url = response.urljoin(entry.xpath("../../../td[3]//a/@href").extract_first())
             self.log_dict[log_url.split('#')[-1].replace('type-', '')] = (log_name, log_desc)
-            yield scrapy.Request(log_url, callback=self.parse_item)
+            yield scrapy.Request(log_url.split('#')[0], callback=self.parse_item)
 
     def parse_item(self, response, **kwargs):
         entries = response.xpath("//dl[@class='zeek type']")
-        log_name, log_desc, field_names, field_descs, exist = '', '', [], [], False
         for entry in entries:
             id = entry.xpath('./dt/@id').extract_first().replace('type-', '')
             if id in self.log_dict:
                 (log_name, log_desc) = self.log_dict[id]
                 field_names = entry.xpath("./dd//dd[@class='field-odd']//dl//dt")
                 field_descs = entry.xpath("./dd//dd[@class='field-odd']//dl//dd")
-                exist = True
-                break
-        if not exist:
-            raise Exception('No Log is Found')
-        for i in range(len(field_names)):
-            item = LogItem()
-            field, desc = field_names[i], field_descs[i]
-            item['log_name'] = log_name
-            item['log_desc'] = log_desc
-            item['field_name'] = field.xpath(".//text()").extract()[0].replace(': ', '')
-            item['field_type'] = ''.join(field.xpath(".//text()").extract()[1:])
-            item['field_desc'] = self.text_clean(''.join(desc.xpath(".//text()").extract()))
-            yield item
+                for i in range(len(field_names)):
+                    item = LogItem()
+                    field, desc = field_names[i], field_descs[i]
+                    item['log_name'] = log_name
+                    item['log_desc'] = log_desc
+                    item['field_name'] = field.xpath(".//text()").extract()[0].replace(': ', '')
+                    item['field_type'] = ''.join(field.xpath(".//text()").extract()[1:])
+                    item['field_desc'] = self.text_clean(''.join(desc.xpath(".//text()").extract()))
+                    yield item
 
     def text_clean(self, s):
         return s.replace('\n', ' ').replace('‘', "'").replace('’', "'").replace('“', '"').replace('”', '"')
